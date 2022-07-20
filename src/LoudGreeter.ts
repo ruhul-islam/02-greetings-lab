ff1e1bbe5d03248c27ef896bac712317d184d13a
ruhul-islam <islamruhul48092@gmail.com>
2022-07-20 10:24:36 -0400

    fixed the exclamation error

--- src/LoudGreeter.ts
+++ src/LoudGreeter.ts
@@ -1,7 +1,7 @@
 import { Greeter } from "./Greeter";
 
 export class LoudGreeter extends Greeter {
-  private extra: string = "!";
+  private extra: string = "!!";
   addVolume(): void {
     this.extra += "!";
   }
--- tests/LoudGreeter.test.ts
+++ tests/LoudGreeter.test.ts
@@ -3,12 +3,12 @@ import { LoudGreeter } from "../src/LoudGreeter";
 describe("LoudGreeter class", () => {
   test("Islamic greeting without calling addVolume", () => {
     const greetLoud: LoudGreeter = new LoudGreeter("Salaam");
-    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!");
+    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!!");
   });
   test("Islamic greeting with calling addVolume once", () => {
     const greetLoud: LoudGreeter = new LoudGreeter("Salaam");
     greetLoud.addVolume();
-    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!!");
+    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!!!");
   });
   test("Islamic greeting with calling addVolume 5 times", () => {
     const greetLoud: LoudGreeter = new LoudGreeter("Salaam");
@@ -17,6 +17,6 @@ describe("LoudGreeter class", () => {
     greetLoud.addVolume();
     greetLoud.addVolume();
     greetLoud.addVolume();
-    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!!!!!!");
+    expect(greetLoud.greet("Mahi")).toBe("Salaam, Mahi!!!!!!!!");
   });
 });
